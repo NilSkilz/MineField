f394bb2522ff3058c5945a3c2ade9c4983e753cd
Rob Stokes <rob@limeninja.com>
2019-03-24 21:05:37 +0000

    Added some tests...

---
+++ MineFieldTests/Game Piece Classes/HeroTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineField.Game_Piece_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineField.Game_Piece_Classes.Tests
+{
+    [TestClass()]
+    public class HeroTests
+    {
+        [TestMethod()]
+        public void LoseLifeTest()
+        {
+            Hero hero = new Hero(3); // Should have 3 lives
+            
+            if (!hero.TakeLife())
+            {
+                // Should return True
+                Assert.Fail("1"); // 2 lives left
+            }
+            if (!hero.TakeLife())
+            {
+                // Should return True
+                Assert.Fail("2"); // 1 life left
+            }
+            if (hero.TakeLife())
+            {
+                // Should return False
+                Assert.Fail("3"); // Game over
+            }
+
+
+            Hero hero2 = new Hero(2); // Should have 3 lives
+
+            if (!hero2.TakeLife())
+            {
+                // Should return True
+                Assert.Fail("4"); // 2 lives left
+            }
+            if (hero2.TakeLife())
+            {
+                // Should return False
+                Assert.Fail("5"); // Game over
+            }
+        }
+    }
+}
--- MineFieldTests/Game Piece Classes/MineTests.cs
+++ MineFieldTests/Game Piece Classes/MineTests.cs
@@ -16,19 +16,64 @@ namespace MineField.Game_Piece_Classes.Tests
         {
             for (int i = 0; i < 100; i++)
             {
-                Mine mine = new Mine(0, 0, new Random());
+                Random rnd = new Random();
+                Mine instance = new Mine(rnd);
+                Coordinate mine = instance.GenerateRandomCoordinate(rnd);
 
-                if (mine.location.X < 2 && mine.location.Y < 2)
+                if (mine.X < 2 && mine.Y < 2)
                 {
-                    Assert.Fail();
+                    Assert.Fail("Less than 2");
                 }
 
-                if (mine.location.X > 6 && mine.location.Y > 6)
+                if (mine.X > 6 && mine.Y > 6)
                 {
-                    Assert.Fail();
+                    Assert.Fail(mine.X.ToString() + " : " + mine.Y.ToString());
                 }
             }
+        }
 
+        [TestMethod()]
+        public void CollisionDetectionTest()
+        {
+            Mine mine = new Mine(new Random());
+            mine.location.X = 3;
+            mine.location.Y = 4;
+
+            if (mine.hasCollided(new Coordinate()
+            {
+                X = 1,
+                Y = 1
+            }))
+            {
+                Assert.Fail();
+            }
+
+            if (mine.hasCollided(new Coordinate()
+            {
+                X = 3,
+                Y = 1
+            }))
+            {
+                Assert.Fail();
+            }
+
+            if (mine.hasCollided(new Coordinate()
+            {
+                X = 1,
+                Y = 4
+            }))
+            {
+                Assert.Fail();
+            }
+
+            if (!mine.hasCollided(new Coordinate()
+            {
+                X = 3,
+                Y = 4
+            }))
+            {
+                Assert.Fail();
+            }
         }
     }
 }
